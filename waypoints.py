2ec86f10a6f2d95fbd598fff57656fa6ca55a5c8
TomGC96 <tom.cartwright@hotmail.co.uk>
2016-02-16 17:06:31 +0000

    Started by adding the waypoint constants to the code

---
+++ waypoints.py
@@ -0,0 +1,17 @@
+# Dewi - Waypoint Navigation - Aber SailBot
+# =========================================
+
+import math
+import boat_utils
+from boatd_client import Boat
+
+# All the waypoints can be added to this list of tuples
+waypoints = [(52.406988,-4.090990)] 
+
+# Use this waypoint for Dewi to return to after completing the course
+home_waypoint = (52.406988,-4.090990) 
+
+# The number of laps of the course to complete before returning home
+laps = 1
+
+dewi = Boat()
