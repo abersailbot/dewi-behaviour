55989f219b8220f347665c87128351f429edfdf3
Daniel <dac46@aber.ac.uk>
2016-05-13 22:05:52 +0100

    move 'waypoint reached' check to the end of the control loop

--- waypoints.py
+++ waypoints.py
@@ -34,12 +34,10 @@ def sail():
 # Function to navigate to the next waypoint
 def navigate(waypoint):
 	# Variable to hold the boolean value of whether the waypoint has been reached or not
-	waypoint_reached = false
+	waypoint_reached = check_waypoint_reached(waypoint)
 
 	# Repeat while the waypoint has not been reached
 	while waypoint_reached == false :
-		# Check whether the waypoint has been reached and whether we should move on to the next waypoint
-		waypoint_reached = check_waypoint_reached(waypoint)
         # Check angle between the wind direction, boat location, and the waypoint - does it require beating
         beat_required = check_boat_waypoint_angle(waypoint)
 
@@ -49,6 +47,9 @@ def navigate(waypoint):
         else
             sail_straight # Next waypoint will be sailed to in a straight line
 
+        # Check whether the waypoint has been reached and whether we should move on to the next waypoint
+        waypoint_reached = check_waypoint_reached(waypoint)
+    
 # Function to beat upwind to the next waypoint
 def sail_beat:
 
