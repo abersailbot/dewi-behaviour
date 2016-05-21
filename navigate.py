fe82dea002170a0beabbcbfc3e4c64ed67034ece
Louis Taylor <louis@kragniz.eu>
2016-05-22 00:11:50 +0100

    Add base navigation class

---
+++ navigate.py
@@ -0,0 +1,24 @@
+import boatdclient
+
+
+class Navigator(object):
+    def __init__(self):
+        self.boat = boatdclient.Boat(convenience=True)
+
+        self.target_point = None
+
+    def set_target_heading(self, angle):
+        '''Set the target angle for the boat.'''
+        pass
+
+    def update(self):
+        '''Update actuators to make progress towards targets.'''
+        pass
+
+    def check_new_target(self):
+        '''
+        Check if a new target point needs to be selected.
+
+        Return a new ``Point`` if target will be changed, None otherwise.
+        '''
+        pass
