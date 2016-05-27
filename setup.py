17dd0e3bcdf488e6d19f4a1fba722110f6d948a1
Louis Taylor <louis@kragniz.eu>
2016-05-27 01:52:17 +0100

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='dewi-behaviour',
+    version='0.1.0',
+    author='Louis Taylor',
+    author_email='louis@kragniz.eu',
+    description=('Boatd behaviours for use with dewi or other similar boats'),
+    license='GPLv3',
+    keywords='boat sailing boatd',
+    url='https://github.com/abersailbot/dewi-behaviour',
+    modules=['navigate'],
+    scripts=[
+        'station-keeping-behaviour',
+        'idle-behaviour',
+        'demo-waypoint-behaviour',
+    ],
+    install_requires=[
+        'python-boatdclient',
+    ],
+)
