d00b388490085918cc010f108d9733b2b33b73c5
Louis Taylor <louis@kragniz.eu>
2016-05-30 18:42:04 +0100

    Clean up syntax

--- navigate.py
+++ navigate.py
@@ -37,8 +37,8 @@ class Navigator(object):
         else:
             target_heading = self.target
 
-#Tacking logic
-        if target_heading < self.boat.wind.direction + Bearing(45) and target_heading > self.boat.wind.direction - Bearing(45):
+        # tacking logic
+        if target_heading < self.boat.wind.direction + Bearing(45) and target_heading > self.boat.wind_direction - Bearing(45):
 
             if self.boat.position.bearing_to(self.target) > 10:
                 target_heading = self.boat.wind.direction + Bearing(45)
@@ -52,8 +52,6 @@ class Navigator(object):
             else:
                 target_heading = self.boat.wind.direction + Bearing(45)
 
-
-
         error = current_heading.delta(target_heading)
         self.integrator += error
         if self.integrator > self.integrator_max:
@@ -62,36 +60,37 @@ class Navigator(object):
         print('heading:', current_heading, '	wanted:', target_heading, '	error:',
               error, '	integrator:', self.integrator, '	target:', self.target)
         self.boat.set_rudder( -(self.k_p * error + self.k_i * self.integrator))
-	self.update_sail()
+        self.update_sail()
 
     def update_sail(self):
-	'''Set the sail to the correct angle based on current wind direction'''
+        '''Set the sail to the correct angle based on current wind direction'''
 
-	relative_wind_direction = self.boat.wind_direction - self.boat.heading
-	if relative_wind_direction < 180:
-			if relative_wind_direction < 70:
-				newSailAngle = 0
-			elif relative_wind_direction < 80:
-				newSailAngle = 18
-			elif relative_wind_direction < 90:
-				newSailAngle = 36
-			elif relative_wind_direction < 110:
-				newSailAngle = 54
-			else:
-				newSailAngle = 72
-		else:
-			if relative_wind_direction >= 290:
-				newSailAngle = 0
-			elif relative_wind_direction >= 280:
-				newSailAngle = 342
-			elif relative_wind_direction >= 270:
-				newSailAngle = 324
-			elif relative_wind_direction >= 250:
-				newSailAngle = 306
-			else:
-				newSailAngle = 288
-				
-		self.boat.set_sail(newSailAngle)
+        relative_wind_direction = self.boat.wind.direction - self.boat.heading
+
+        if relative_wind_direction < 180:
+            if relative_wind_direction < 70:
+                sail_angle = 0
+            elif relative_wind_direction < 80:
+                sail_angle = 18
+            elif relative_wind_direction < 90:
+                sail_angle = 36
+            elif relative_wind_direction < 110:
+                sail_angle = 54
+            else:
+                sail_angle = 72
+        else:
+            if relative_wind_direction >= 290:
+                sail_angle = 0
+            elif relative_wind_direction >= 280:
+                sail_angle = 342
+            elif relative_wind_direction >= 270:
+                sail_angle = 324
+            elif relative_wind_direction >= 250:
+                sail_angle = 306
+            else:
+                sail_angle = 288
+
+        self.boat.set_sail(sail_angle)
 
     def run(self):
         '''
