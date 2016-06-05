92e93f4794824b0de1d59297efbc26d298ae1216
Lizzie <lizzie.stone@hotmail.com>
2016-06-05 14:10:33 -0400

    Change taking to include exactly 180

--- navigate.py
+++ navigate.py
@@ -58,13 +58,13 @@ class Navigator(object):
                 pass
 
             if bearing_to_wind > cone_angle and bearing_to_wind < (360 - cone_angle):
-                if bearing_to_wind < 180:
+                if bearing_to_wind <= 180:
                     target_heading = self.boat.wind.direction + Bearing(45)
                 if bearing_to_wind > 180:
                     target_heading = self.boat.wind.direction - Bearing(45)
 
             elif bearing_to_wind < cone_angle and bearing_to_wind > (360 - cone_angle):
-                if bearing_to_wind < 180:      
+                if bearing_to_wind <= 180:      
                     target_heading = self.boat.wind.direction - Bearing(45)
                 if bearing_to_wind > 180:
                     target_heading = self.boat.wind.direction + Bearing(45)
